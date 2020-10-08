83447439c20a2ac5002e85a498b7934bf48fff6f
Imran Desai <imran.desai@intel.com>
2020-10-07 19:23:03 -0700

    .ci : 4.X backwards compatibility test to ensure options don't break
    Fixes #2188
    
    Signed-off-by: Imran Desai <imran.desai@intel.com>

--- scripts/utils/v4X_compatibility_test.sh
+++ scripts/utils/v4X_compatibility_test.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
+set -eo pipefail
+
 # Checkout 4.X integration test directory
-make clean; make distclean
-git stash
-curr_branch=$(git status | grep "On branch" | sed 's/On branch//g')
-testcompatbranch=$RANDOM
-git checkout 4.3.0
-git checkout -b $testcompatbranch
-git checkout $curr_branch
-git checkout $testcompatbranch test/integration/tests
-git branch -D $testcompatbranch
+git stash -q
+curr_branch=$(git log HEAD --pretty=oneline --abbrev-commit| head -n 1|awk  '{print $1}')
+git checkout -b $curr_branch -q
+git fetch origin --tags -q
+git checkout 4.3.0 -q
+git checkout -b 4.3.0 -q
+git checkout $curr_branch -q
+git checkout 4.3.0 test/integration/tests -q
+git branch -D 4.3.0 -q
 
 #
 # RSA OAEP decryption was enabled as a feature after 4.X and so testing it
 # would signal a false compatibility test failure.
 #
-git checkout HEAD test/integration/tests/rsadecrypt.sh
+git checkout HEAD test/integration/tests/rsadecrypt.sh && \
 #
 # tpm2_getekcertificate is known to break backwards compatibility
 #
-git checkout HEAD test/integration/tests/getekcertificate.sh
+git checkout HEAD test/integration/tests/getekcertificate.sh && \
+#
+# symlink is an irrelevant test for 4.X branch
+#
+rm test/integration/tests/symlink.sh && \
+echo "v4X compatibility test follows" && \
+echo "..."
 
 #
 # Beyond 4.X release, the tpm2-tools were combined into a single
 # busybox style binary "tpm2". The following makes adjustments
 # to the tool-name which essentially invokes the same tools.
 #
-# Also, symlink is an irrelevant test for 4.X branch.
 #
-for f in `find test/integration/tests -iname '*.sh' | grep -v symlink`
+for f in `find test/integration/tests -iname '*.sh'`
 do
     for i in `find tools -iname 'tpm2*.c'`
     do
@@ -38,4 +45,11 @@ do
     done
 done
 
-./bootstrap; ./configure --enable-unit; make -j8 ; make check -j8
+mkdir compatibility_testbuild && \
+pushd compatibility_testbuild && \
+../configure --enable-unit --disable-fapi --disable-hardening && \
+set -x && \
+make -j$(nproc) && \
+make check -j$(nproc)
+popd
+git reset --hard HEAD
